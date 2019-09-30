02a2e9289f3bdf2233e69e79c8e575e0651a9374
Daniele Bellavist <daniele.bellavista@protonmail.com>
2019-10-01 01:12:48 +0200

    Media label importer

--- index.js
+++ index.js
@@ -9,6 +9,8 @@
 
 const {ProtonMail} = require('./lib/protonmail');
 const prompt = require('prompt');
+const cheerio = require('cheerio');
+const url = require('url');
 
 async function main() {
   prompt.start();
@@ -33,6 +35,107 @@ async function main() {
     twofa: true,
   });
   await pm.login();
+  const newMusic = new Set();
+  const addMusic = (music) => {
+    if (!newMusic.has(music)) {
+      newMusic.add(music);
+      console.log(music);
+    }
+  };
+  const {conversations} = await pm.getConversations({
+    where: 'Media',
+    page: 0,
+  });
+  for (const conversation of conversations) {
+    if (conversation.messages.unread > 0) {
+      await pm.getConversation(conversation);
+      for (const message of conversation.messages) {
+        if (!message.unread) {
+          continue;
+        }
+        let parsed = false;
+        await pm.getMessageBody(message);
+        const $ = cheerio.load(message.decryptedBody);
+        // TODO parse other
+        if (message.sender.name === 'Bandcamp') {
+          $('a[href*="bandcamp.com/album"],a[href*="bandcamp.com/track"]').each(
+              function() {
+                parsed = true;
+                const el = $(this);
+                const music = el.attr('href');
+                const uobj = url.parse(music);
+                delete uobj.search;
+                delete uobj.hash;
+                delete uobj.query;
+                addMusic(url.format(uobj));
+              }
+          );
+        } else if (message.sender.address === 'noreply@mixcloudmail.com') {
+          if (message.subject === 'Weekly Update') {
+            $('a').each(function() {
+              const el = $(this);
+              if (el.text() === 'Play All') {
+                parsed = true;
+                addMusic(el.attr('href'));
+              }
+            });
+          } else {
+            let found = false;
+            const links = [];
+            let titlee = /"(.*)" uploaded by/i.exec(message.subject);
+            if (!titlee) {
+              titlee = /is sharing exclusive shows - (.*) By /i.exec(
+                  message.subject
+              );
+            }
+            if (titlee) {
+              const title = titlee[1].toLowerCase().slice(0, 50);
+
+              $('a').each(function() {
+                const el = $(this);
+                links.push(el.text());
+                if (
+                  el
+                      .text()
+                      .toLowerCase()
+                      .indexOf(title) >= 0
+                ) {
+                  addMusic(el.attr('href'));
+                  found = true;
+                }
+              });
+            } else {
+              $('a').each(function() {
+                const el = $(this);
+                links.push(el.text());
+              });
+            }
+            parsed = found;
+            if (!found) {
+              console.log(
+                  JSON.stringify(
+                      {
+                        notFound: message.subject,
+                        titlee,
+                        links,
+                      },
+                      null,
+                      ' '
+                  )
+              );
+            }
+          }
+        } else {
+          console.log(JSON.stringify(message, null, ' '));
+        }
+        if (parsed) {
+          await pm.markAsRead(message);
+        }
+      }
+    }
+  }
+
+  await pm.logout();
 }
 
 main();
