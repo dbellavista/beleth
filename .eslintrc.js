9f9bb8cf3efa4f7babcb413b877243fa04070e3a
Daniele Bellavista <daniele.bellavista@protonmail.com>
2020-06-13 12:42:04 +0200

    Usign new pattern

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,68 @@
 module.exports = {
-  env: {
-    es6: true,
-    node: true
-  },
-  extends: ['google'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly'
-  },
-  parser: 'babel-eslint',
+  root: true,
   parserOptions: {
     ecmaVersion: 2020,
-    sourceType: 'script'
+    sourceType: "script",
+  },
+  env: {
+    es6: true,
+    node: true,
+  },
+  plugins: ["jsdoc", "prettier"],
+  extends: [
+    "google",
+    "plugin:jsdoc/recommended",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
   rules: {
-    'require-jsdoc': 'off',
-    'no-invalid-this': 'off',
-    'no-undef': 'error',
-    'no-const-assign': 'error'
-  }
+    "prettier/prettier": "error",
+    "require-jsdoc": "off",
+    "valid-jsdoc": "off",
+    // Mongoose and such
+    "no-invalid-this": "off",
+    "no-undef": "error",
+    "no-console": "error",
+    "no-const-assign": "error",
+    "no-unused-vars": [
+      "error",
+      { varsIgnorePattern: "^logger$", args: "none" },
+    ],
+    // using tsserver for this
+    "jsdoc/no-undefined-types": "off",
+    "jsdoc/valid-types": "off",
+    "jsdoc/check-types": "off",
+    "jsdoc/check-tag-names": [
+      "error",
+      {
+        definedTags: [
+          "template",
+          "api",
+          "apiErrorExample",
+          "apidefine",
+          "apiExample",
+          "apiGroup",
+          "apiName",
+          "apiParam",
+          "apiDescription",
+          "apiSuccessExample",
+          "apiHeader",
+        ],
+      },
+    ],
+    "require-yield": "error",
+    "max-len": [
+      "error",
+      {
+        code: 100,
+        ignoreComments: true,
+        ignorePattern: /( = require\()|(^\s*(return\s*)?'.*'[,;]?$)/.source,
+        ignoreUrls: true,
+        tabWidth: 2,
+      },
+    ],
+  },
 };
--- index.js
+++ index.js
@@ -2,7 +2,6 @@
  * @file index.js
  * @module protonmail-api/index
  * @author Daniele Bellavista
- * @version 0.0
  *
  */
 'use strict';
@@ -13,6 +12,9 @@ const fsPromises = require('fs').promises;
 const MusicLinkGetter = require('./lib/music-link-getter');
 const ReadingInfoGetter = require('./lib/reading-info-getter');
 
+/**
+ *
+ */
 async function main() {
   const musicDest = await fsPromises.open(process.argv[2], 'wx');
   const readingDest = await fsPromises.open(process.argv[3], 'wx');
@@ -20,18 +22,18 @@ async function main() {
   prompt.start();
   const {user, password} = await new Promise((resolve, reject) =>
     prompt.get(
-        [
-          {
-            name: 'user',
-            required: true,
-          },
-          {
-            name: 'password',
-            hidden: true,
-          },
-        ],
-        (err, result) => (err ? reject(err) : resolve(result)),
-    ),
+      [
+        {
+          name: 'user',
+          required: true,
+        },
+        {
+          name: 'password',
+          hidden: true,
+        },
+      ],
+      (err, result) => (err ? reject(err) : resolve(result))
+    )
   );
   const pm = new ProtonMail({
     user,
@@ -41,42 +43,42 @@ async function main() {
   await pm.login();
 
   const mlinkGetter = new MusicLinkGetter(
-      pm,
-      async (links) => {
-        if (links.length > 0) {
-          await musicDest.write(Buffer.from(`${links.join('\n')}\n`));
-        }
-      },
-      (info) => {
-        console.log(JSON.stringify(info, null, ' '));
-      },
+    pm,
+    async (links) => {
+      if (links.length > 0) {
+        await musicDest.write(Buffer.from(`${links.join('\n')}\n`));
+      }
+    },
+    (info) => {
+      console.log(JSON.stringify(info, null, ' '));
+    }
   );
   await mlinkGetter.task();
   await musicDest.close();
 
   const rInfoGetter = new ReadingInfoGetter(
-      pm,
-      async (info) => {
-        if (info.length === 0) {
-          return;
-        }
-        const stringToWrite = info
-            .map((ri) => {
-              return `
+    pm,
+    async (info) => {
+      if (info.length === 0) {
+        return;
+      }
+      const stringToWrite = info
+        .map((ri) => {
+          return `
 ${ri.title}
   ${ri.url}
   ${ri.description
-      .replace(/\n/g, ' ')
-      .replace(/(.{76}\S+)\s*/g, '$1\n')
-      .replace(/\n/g, '\n  ')
-      .trim()}`;
-            })
-            .join('\n');
-        await readingDest.write(Buffer.from(stringToWrite + '\n\n'));
-      },
-      (info) => {
-        console.log(JSON.stringify(info, null, ' '));
-      },
+    .replace(/\n/g, ' ')
+    .replace(/(.{76}\S+)\s*/g, '$1\n')
+    .replace(/\n/g, '\n  ')
+    .trim()}`;
+        })
+        .join('\n');
+      await readingDest.write(Buffer.from(stringToWrite + '\n\n'));
+    },
+    (info) => {
+      console.log(JSON.stringify(info, null, ' '));
+    }
   );
   try {
     await rInfoGetter.task();
